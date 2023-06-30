9c73a04eba2d60f1b69f31fb3d940cfaffcc695d
datata <dani@danitarazona.dev>
2023-06-30 10:23:08 +0200

    feat: fecth ghibli films

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [films, setFilms] = useState([])
+
+  useEffect(() => {
+    fetch('https://ghibliapi.vercel.app/films')
+      .then(res => res.json())
+      .then(res => setFilms(res))
+      .catch(error => console.log(error))
+
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {films.map(film => {
+        return(
+          <div key={film.id}>
+            <h1>{film.original_title}</h1>
+            <img src={film.image} alt="" width={300}/>
+          </div>
+        )
+      })}
     </>
   )
 }
